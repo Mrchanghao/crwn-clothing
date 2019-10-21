b0b0f9c072721b187ae9d1c930502d2d4335c1ad
이창호 <changhao@ichanghoui-MacBookPro.local>
2019-10-21 18:45:13 +0900

    redux initial store setup

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './redux/store'
 
-const app = <Router><App/></Router>
+const app = (
+  <Provider store={store}>
+    <Router><App/></Router>
+  </Provider>
+)
 
 
 ReactDOM.render(app, document.getElementById('root'));
